15ece8a586ee1106960347501aa70a718b532e2f
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-07-07 12:21:30 +0300

    first commit

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word.downcase
+  end
+
+  def match(words)
+    words.select { |w| w.downcase.chars.sort == word.chars.sort && w.downcase != word }
+  end
+end
